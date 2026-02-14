a80a2ed553756e0b41686ddfe7f570d3cc818642
JElliott84 <164680937+JElliott84@users.noreply.github.com>
2026-02-14 15:01:33 -0500

    Update Home.js
    Fixed issue with coding on return and also changed the links so they are working.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 
-export default function Home() {
-  return (
+function Home() {
+ return (
     <div className="page">
       <h1>Welcome to Xavier Institute</h1>
-      <p>Select an option below.</p>
+      <p>We fully expect to be live and running by December 2099!</p>
 
-      <div className="homeButtons">
-        <Link className="btn" to="/courses">Manage Courses</Link>
-        <Link className="btn" to="/about">About</Link>
+      
       </div>
-    </div>
+  
   );
 }
+export default Home;
